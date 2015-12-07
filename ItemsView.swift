a941215bd407dd46bd393322c4bc137ab018b55b
cecilia <cecilia@ceciliatekiMacBook-Air.local>
2015-12-08 00:48:04 +0800

    init for woqu

---
+++ ItemsView.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsView.swift
+//  woqu
+//
+//  Created by cecilia on 15/10/7.
+//  Copyright © 2015年 lei.han. All rights reserved.
+//
+
+import Cocoa
+
+class ItemsView: UITableView {
+
+}
---
+++ woqu/ItemUITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemUITableViewCell.swift
+//  woqu
+//
+//  Created by cecilia on 15/10/12.
+//  Copyright © 2015年 lei.han. All rights reserved.
+//
+
+import UIKit
+
+class ItemUITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
